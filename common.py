4a17bf003eeb9695a47bde31fb36e2c5043e504e
James Paterson <james@mjsquare.com>
2018-03-07 18:47:36 +0000

    seperate into two files

---
+++ common.py
@@ -0,0 +1,118 @@
+from nltk.corpus import stopwords
+from keras.preprocessing.text import text_to_word_sequence
+from functools import reduce
+import os 
+
+loc = os.path.dirname(os.path.realpath(__file__))
+
+DATE_STRING_LIST = [
+    "jan", "january", "feb", "febuary", "mar", "march", "apr", "april", "jun", "june", "jul", "july", "aug", "august", "sep", "september",
+    "oct", "october", "nov", "november", "dec", "december"
+]
+FILE_ROOT_PATH = loc+'/files'
+
+
+def loadArticle(id):
+    a = open(FILE_ROOT_PATH+'/Doc '+str(id)+".txt", 'r', encoding='utf8');
+    return a.read()+ ' '
+
+def splitSentences(text):
+    text = text.replace("\n", " ")
+    text = text.replace("\"", "")
+    return text.split('. ')
+
+def preprocess(text):
+    # Split to sentences
+    sentences = splitSentences(text)
+    
+    stops = set(stopwords.words("english"))
+    
+    # Remove stops
+    for i in range(0, len(sentences)):
+        sentences[i] = text_to_word_sequence(sentences[i])
+        sentences[i] = [w for w in sentences[i] if not w in stops]
+        sentences[i] = " ".join(sentences[i])
+
+    # Remove empty values
+    sentences = list(filter(len, sentences))
+
+    return sentences
+
+
+def calculateSentenceWeight(sentence, frequency, terms):
+    # Split into array
+    sentence = text_to_word_sequence(sentence)
+
+    # Calculate weights
+    weight = 0
+    for word in sentence:
+        try:
+            # Lookup term
+            index = terms.index(word)
+            weight += frequency[index]
+
+            # Increase weight based on word content
+
+            # Numbers are probably important
+            if word.isdigit():
+                weight += 1
+                # Years are probably even more important
+                if len(word) == 4:
+                    weight += 1
+
+            # Dates are probably important as well
+            if word in DATE_STRING_LIST:
+                weight += 1
+        except ValueError:
+            # Word was not in our vocab
+            pass
+    
+    # Normalise by sentence size
+    return weight/len(sentence)
+
+def summarize(original, weightFunction, length=500):
+    # Init weights and sentences
+    weights = weightFunction(original)
+    sentences = splitSentences(original)
+
+    # Build our summary
+    summarySentences = [] 
+    while(
+        reduce(lambda a, b: a + len(b[1]), summarySentences, 0) + len(sentences[weights[0][0]]) < length
+    ):
+        # Pick the highest weight sentence from the article
+        summarySentences.append((
+            weights[0][0],
+            sentences[weights[0][0]]
+        ))
+
+        # Remove that sentence
+        sentences.pop(weights[0][0])
+
+        # Regenerate article
+        article = ". ".join(sentences)
+        weights = weightFunction(article)
+        sentences = splitSentences(article)
+
+    # Fill any more unused space
+    i = 0
+    while(
+        reduce(lambda a, b: a + len(b[1]), summarySentences, 0) + len(sentences[weights[i][0]]) < length
+    ):
+        summarySentences.append((
+            weights[0][0],
+            sentences[weights[0][0]]
+        ))
+
+    # Sort sentences by the position they appeared in the document
+    summarySentences.sort(
+        key=lambda a: a[0]
+    )
+
+    # Build the summary
+    summary = reduce(
+        lambda a, b: a + b[1] + '. ',
+        summarySentences,
+        ""
+    )
+    return summary
--- tf.py
+++ tf.py
@@ -1,50 +1,14 @@
-# Deeplearning 
-from keras.preprocessing.text import text_to_word_sequence
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from nltk.corpus import stopwords
+# Tf stuff 
+from sklearn.feature_extraction.text import CountVectorizer
 
 # Other external libs
 import numpy as np
 
-# Standard libs
-from functools import reduce
-import os 
-
-loc = os.path.dirname(os.path.realpath(__file__))
+# From common
+from common import loadArticle, splitSentences, preprocess, calculateSentenceWeight, summarize
 
 # CONSTANTS
-FILE_ROOT_PATH = loc+'/files'
 SUMMARY_LENGTH = 500
-DATE_STRING_LIST = [
-    "jan", "january", "feb", "febuary", "mar", "march", "apr", "april", "jun", "june", "jul", "july", "aug", "august", "sep", "september",
-    "oct", "october", "nov", "november", "dec", "december"
-]
-
-def loadArticle(id):
-    a = open(FILE_ROOT_PATH+'/Doc '+str(id)+".txt", 'r', encoding='utf8');
-    return a.read()+ ' '
-
-def splitSentences(text):
-    text = text.replace("\n", " ")
-    text = text.replace("\"", "")
-    return text.split('. ')
-
-def preprocess(text):
-    # Split to sentences
-    sentences = splitSentences(text)
-    
-    stops = set(stopwords.words("english"))
-    
-    # Remove stops
-    for i in range(0, len(sentences)):
-        sentences[i] = text_to_word_sequence(sentences[i])
-        sentences[i] = [w for w in sentences[i] if not w in stops]
-        sentences[i] = " ".join(sentences[i])
-
-    # Remove empty values
-    sentences = list(filter(len, sentences))
-
-    return sentences
 
 def calculateTermFrequency(corpus, max_features=None, ngrams=1, vocabulary=None):
     '''
@@ -61,49 +25,6 @@ def calculateTermFrequency(corpus, max_features=None, ngrams=1, vocabulary=None)
     )
     features = vectorizer.fit_transform(corpus)
     return (features.toarray(), vectorizer.get_feature_names())
-
-def calculateSentenceWeight(sentence, frequency, terms):
-    # Split into array
-    sentence = text_to_word_sequence(sentence)
-
-    # Calculate weights
-    weight = 0
-    for word in sentence:
-        try:
-            # Lookup term
-            index = terms.index(word)
-            weight += frequency[index]
-
-            # Increase weight based on word content
-
-            # Numbers are probably important
-            if word.isdigit():
-                weight += 1
-                # Years are probably even more important
-                if len(word) == 4:
-                    weight += 1
-
-            # Dates are probably important as well
-            if word in DATE_STRING_LIST:
-                weight += 1
-        except ValueError:
-            # Word was not in our vocab
-            pass
-    
-    # Normalise by sentence size
-    return weight/len(sentence)
-
-def formatSummary(weightArray, originalText, length=500):
-    sentences = splitSentences(originalText)
-    result = ''
-    i = 0
-    while(
-        len(result) + len(sentences[weightArray[i][0]]) < length
-    ):
-        result += sentences[weightArray[i][0]] + '. '
-        i += 1
-
-    return result
 
 def tf(text):
     article = preprocess(text)
@@ -125,58 +46,9 @@ def tf(text):
     weightArray.reverse()
     return weightArray
 
-def summarize(original):
-    # Init weights and sentences
-    weights = tf(original)
-    sentences = splitSentences(original)
-
-    # Build our summary
-    summarySentences = [] 
-    while(
-        reduce(lambda a, b: a + len(b[1]), summarySentences, 0) + len(sentences[weights[0][0]]) < SUMMARY_LENGTH
-    ):
-        # Pick the highest weight sentence from the article
-        summarySentences.append((
-            weights[0][0],
-            sentences[weights[0][0]]
-        ))
-
-        # Remove that sentence
-        sentences.pop(weights[0][0])
-
-        # Regenerate article
-        article = ". ".join(sentences)
-        weights = tf(article)
-        sentences = splitSentences(article)
-
-    # Fill any more unused space
-    i = 0
-    while(
-        reduce(lambda a, b: a + len(b[1]), summarySentences, 0) + len(sentences[weights[i][0]]) < SUMMARY_LENGTH
-    ):
-        summarySentences.append((
-            weights[0][0],
-            sentences[weights[0][0]]
-        ))
-
-    # Sort sentences by the position they appeared in the document
-    summarySentences.sort(
-        key=lambda a: a[0]
-    )
-
-    print(summarySentences)
-
-    # Build the summary
-    summary = reduce(
-        lambda a, b: a + b[1] + '. ',
-        summarySentences,
-        ""
-    )
-    return summary
-
 for i in range(1, 9):
     original = loadArticle(i)
-    summary = summarize(original)
+    summary = summarize(original, tf, length=SUMMARY_LENGTH)
 
     print()
     print('Article', i)
